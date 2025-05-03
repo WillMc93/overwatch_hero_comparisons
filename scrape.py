0a81c8a765d502dad6bdd55cc277d02c5e09968a
Will McElhenney <wmcelhenney93@gmail.com>
2025-05-03 17:40:47 -0500

    Enforce quotation mark ideology

--- scrape.py
+++ scrape.py
@@ -11,8 +11,8 @@ from bs4 import BeautifulSoup
 
 
 # Constants
-BASE_URL = "https://overwatch.fandom.com/wiki/"
-ROSTER_URL = f"{BASE_URL}Template:Heroes"
+BASE_URL = 'https://overwatch.fandom.com/'
+ROSTER_URL = f'{BASE_URL}/wiki/Template:Heroes'
 
 # %% Functions
 def get_roster() -> pd.DataFrame:
@@ -24,25 +24,25 @@ def get_roster() -> pd.DataFrame:
     time.sleep(2) # Be nice to the server
     if page.status_code != 200:
         raise Exception(f"Failed to load page: {ROSTER_URL}")
-    soup = BeautifulSoup(page.content, "html.parser")
+    soup = BeautifulSoup(page.content, 'html.parser')
 
     # Find the heros
-    table = soup.find("table", {"class": "navbox"})
-    table = table.find("tbody")
-    hero_td = table.find_all("td")
+    table = soup.find('table', {'class': 'navbox'})
+    table = table.find('tbody')
+    hero_td = table.find_all('td')
 
     # Get the link to the hero pages
     hero_links = []
     for td in hero_td:
-        link = td.find("a")
-        if link is None or "href" not in link.attrs:
+        link = td.find('a')
+        if link is None or 'href' not in link.attrs:
             continue
         hero_links.append(link)
 
     # Get the hero names and urls
     df = pd.DataFrame(
-        [(link['title'], link["href"]) for link in hero_links],
-        columns=["name", "url"],
+        [(link['title'], link['href']) for link in hero_links],
+        columns=['name', 'url'],
     )
 
     return df
